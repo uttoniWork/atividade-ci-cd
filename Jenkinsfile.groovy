76da554ec7e27e413089f5ececf737dfa535c4ed
ualfredo <ualfredo@uolinc.com>
2022-11-03 20:06:30 -0300

    Adicionando script e Jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                echo 'Building..'
+                sh '''
+                    cd seminario-C214
+                    npm build
+                   '''
+            }
+        }
+        stage('Test') {
+            steps {
+                echo 'Testing..'
+                sh '''
+                    cd seminario-C214
+                    npm test
+                   '''
+                   archiveArtifacts 'seminario-C214/target/site/'
+            }
+        }
+        stage('Notification') {
+            steps {
+                echo 'Sending email....'
+                sh '''
+                    cd scripts/
+                    chmod 775 *
+                    ./script-email.sh
+                   '''
+            }
+        }
+    }
+}
---
+++ scripts/script-email.sh
@@ -0,0 +1,1 @@
+echo "Mandando e-mail com o mail do linux" | mail -s "subject: qualquer coisa" camila.silva@ges.inatel.br
